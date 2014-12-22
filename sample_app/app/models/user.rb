d170aeb89f73e6453bd4940916a820fc3c49aab9
enixdark <cqshinn92@gmail.com>
2014-12-23 01:39:39 +0700

    add password field

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -2,15 +2,34 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  has_secure_password
+  attr_accessible :email, :name,:password, :password_confirmation
+  attr_accessor :password, :password_confirmation
 
-  validates :name, presence: true
+  validates_presence_of :password, :on => :create
+  before_save { |user| user.email = email.downcase }
+
+  REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: {with: REGEX_EMAIL}, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: {maximum: 30,minimum: 6}
+  # validates :password, presence: true, length: {maximum: 30,minimum: 6}, confirmation: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  def password=(pass)
+    @password = pass
+    unless pass.blank?
+      self.password_digest = BCrypt::Password.create(pass)
+    end
+  end
+
 end
