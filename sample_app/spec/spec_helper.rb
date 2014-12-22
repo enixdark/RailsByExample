5daaebbb6741e4381a7481341d8728147bcb9a6b
enixdark <cqshinn92@gmail.com>
2014-12-22 12:35:15 +0700

    update sign_in

--- sample_app/spec/spec_helper.rb
+++ sample_app/spec/spec_helper.rb
@@ -7,10 +7,10 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] ||= 'test'
-  unless defined?(Rails)
-    require File.expand_path("../../config/environment", __FILE__)
-  end
+  # ENV["RAILS_ENV"] ||= 'test'
+  # unless defined?(Rails)
+  #   require File.expand_path("../../config/environment", __FILE__)
+  # end
 end
 
 Spork.each_run do
@@ -50,8 +50,8 @@ end
 
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-# ENV["RAILS_ENV"] ||= 'test'
-# require File.expand_path("../../config/environment", __FILE__)
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
